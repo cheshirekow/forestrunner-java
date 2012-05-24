a903adace47bdeed4ffa19c6e3e87a4444d64765
Josh Bialkowski <jbialk@mit.edu>
2012-05-24 18:23:40 -0400

    Implemented user and global score retreval

--- src/edu/mit/lids/ares/forestrunner/data/stores/AppletStore.java
+++ src/edu/mit/lids/ares/forestrunner/data/stores/AppletStore.java
@@ -1,11 +1,24 @@
 package edu.mit.lids.ares.forestrunner.data.stores;
 
+
+
 import java.applet.Applet;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+import org.json.simple.JSONObject;
 
 import netscape.javascript.JSException;
 import netscape.javascript.JSObject;
@@ -14,7 +27,9 @@ import com.jme3.app.AppletHarness;
 
 import edu.mit.lids.ares.forestrunner.AdvancedSettings;
 import edu.mit.lids.ares.forestrunner.Game;
+import edu.mit.lids.ares.forestrunner.data.GlobalHighScoreRow;
 import edu.mit.lids.ares.forestrunner.data.Store;
+import edu.mit.lids.ares.forestrunner.data.UserHighScoreRow;
 
 
 /**
@@ -201,5 +216,287 @@ public class AppletStore
         {
             e.printStackTrace(System.err);
         }
+    }
+    
+    @Override
+    public void sendNick()
+    {
+        try
+        {
+            Map<String,String> paramMap = new HashMap<String,String>();
+            paramMap.put("hash", getString("hash"));
+            paramMap.put("nick", getString("nick"));
+            
+            String urlString = 
+                    "http://ares.lids.mit.edu/~jbialk/forest_runner/src/" 
+                    + Store.encode("set_nick.php", paramMap);
+    
+            // try to open a stream to the create user page
+            InputStream source = new URL(urlString).openStream();
+        
+            // read the entire stream into a single string
+            String jsonString = 
+                    new Scanner( source, "UTF-8" )
+                            .useDelimiter("\\A").next();
+    
+            JSONObject obj = (JSONObject) JSONValue.parse(jsonString);
+            
+            // check the result message
+            if( !obj.containsKey("status") )
+            {
+                System.err.println("Returned JSON message is malformed, " +
+                                    "no status: " + jsonString);
+                return;
+            }
+            
+            String status = (String) obj.get("status");
+            if( status.compareTo("OK") != 0 )
+            {
+                System.err.println("Failed to update nickname with server ");
+                if(obj.containsKey("message"))
+                    System.err.println("Message: " + obj.get("message"));
+                return;
+            }
+            
+            System.out.println("Updated nickname at server: " + jsonString);
+        }
+        
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace(System.err);
+        } 
+        
+        catch (IOException e)
+        {
+            e.printStackTrace(System.err);
+        }
+    }
+    
+    @Override
+    public void recordScore(float score)
+    {
+        try
+        {
+            Map<String,String> paramMap = new HashMap<String,String>();
+            paramMap.put("date",    String.format("%d",System.currentTimeMillis()/1000));
+            paramMap.put("velocity",String.format("%d",getInteger("velocity")));
+            paramMap.put("density", String.format("%d",getInteger("density")));
+            paramMap.put("radius",  String.format("%d",getInteger("radius")));
+            paramMap.put("score",   String.format("%.30f",score));
+            paramMap.put("hash",    getString("hash"));
+            paramMap.put("version", String.format("%d",getInteger("version")));
+                
+            String urlString = 
+                    "http://ares.lids.mit.edu/~jbialk/forest_runner/src/" 
+                    + Store.encode("insert_score_applet.php", paramMap);
+
+            // try to open a stream to the create user page
+            InputStream source = new URL(urlString).openStream();
+        
+            // read the entire stream into a single string
+            String jsonString = 
+                    new Scanner( source, "UTF-8" )
+                            .useDelimiter("\\A").next();
+
+            JSONObject obj = (JSONObject) JSONValue.parse(jsonString);
+            
+            // check the result message
+            if( !obj.containsKey("status") )
+            {
+                System.err.println("Returned JSON message is malformed, " +
+                                    "no status: " + jsonString);
+                return;
+            }
+            
+            String status = (String) obj.get("status");
+            if( status.compareTo("OK") != 0 )
+            {
+                System.err.println("Failed to send scores to server ");
+                if(obj.containsKey("message"))
+                    System.err.println("Message: " + obj.get("message"));
+                return;
+            }
+            
+            setInteger("lastRowId", ( (Long)obj.get("row_id") ).intValue());
+            
+            System.out.println("Sent score to server: " + jsonString);
+        }
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace(System.err);
+        } 
+        catch (IOException e)
+        {
+            e.printStackTrace(System.err);
+        }
+    }
+    
+    @Override
+    public List<UserHighScoreRow>   getUserScores()
+    {
+        List<UserHighScoreRow> scores = new ArrayList<UserHighScoreRow>();
+        
+        Map<String,String> paramMap = new HashMap<String,String>();
+        paramMap.put("version", String.format("%d",getInteger("version")));
+        paramMap.put("velocity", String.format("%d",getInteger("velocity")));
+        paramMap.put("density", String.format("%d",getInteger("density")));
+        paramMap.put("radius", String.format("%d",getInteger("radius")));
+        paramMap.put("hash",   getString("hash"));
+        
+        try
+        {
+            String urlString = 
+                    "http://ares.lids.mit.edu/~jbialk/forest_runner/src/" 
+                    + Store.encode("get_user_scores_applet.php", paramMap);
+    
+            // try to open a stream to the create user page
+            InputStream source = new URL(urlString).openStream();
+    
+            // read the entire stream into a single string
+            String jsonString = 
+                    new Scanner( source, "UTF-8" )
+                            .useDelimiter("\\A").next();
+    
+            JSONObject obj = (JSONObject) JSONValue.parse(jsonString);
+            
+            // check the result message
+            if( !obj.containsKey("status") )
+            {
+                System.err.println("Returned JSON message is malformed, " +
+                                    "no status: " + jsonString);
+                throw new RuntimeException("");
+            }
+            
+            String status = (String) obj.get("status");
+            if( status.compareTo("OK") != 0 )
+            {
+                System.err.println("Failed to get global scores from server ");
+                if(obj.containsKey("message"))
+                    System.err.println("Message: " + obj.get("message"));
+                throw new RuntimeException("");
+            }
+            
+            // otherwise, things look good
+            JSONArray scoreArray = (JSONArray)obj.get("scores");
+            
+            System.out.println(
+                    "Received user scores: (" + scoreArray.size() + ")");
+            
+            for( Object scoreObj : scoreArray )
+            {
+                JSONObject scoreMap = (JSONObject)scoreObj;
+                
+                System.out.println(scoreMap.toJSONString());
+                UserHighScoreRow row = new UserHighScoreRow();
+                row.id      = Integer.parseInt(     (String) scoreMap.get("row_id") );
+                row.date    = Integer.parseInt(     (String) scoreMap.get("date") );
+                row.score   = Double.parseDouble(   (String)  scoreMap.get("score") );
+                
+                if( row.id == getInteger("lastRowId"))
+                    row.isCurrent = true;
+                
+                scores.add(row);
+            }
+        
+        }
+        catch (MalformedURLException e)
+        {
+            System.err.println("Failed to get global scores form server");
+            e.printStackTrace(System.out);
+        } 
+        catch (IOException e)
+        {
+            System.err.println("Failed to get global scores form server");
+            e.printStackTrace(System.out);
+        }  
+        catch( RuntimeException e)
+        {
+            System.err.println("Failed to process row");
+            e.printStackTrace(System.err);
+        }
+
+        
+        return scores;
+    }
+    
+    
+    @Override
+    public List<GlobalHighScoreRow>   getGlobalScores()
+    {
+        List<GlobalHighScoreRow> scores = new ArrayList<GlobalHighScoreRow>();
+        
+        Map<String,String> paramMap = new HashMap<String,String>();
+        paramMap.put("version", String.format("%d",getInteger("version")));
+        paramMap.put("velocity", String.format("%d",getInteger("velocity")));
+        paramMap.put("density", String.format("%d",getInteger("density")));
+        paramMap.put("radius", String.format("%d",getInteger("radius")));
+        
+        try
+        {
+            String urlString = 
+                    "http://ares.lids.mit.edu/~jbialk/forest_runner/src/" 
+                    + Store.encode("get_global_scores_applet.php", paramMap);
+    
+            // try to open a stream to the create user page
+            InputStream source = new URL(urlString).openStream();
+    
+            // read the entire stream into a single string
+            String jsonString = 
+                    new Scanner( source, "UTF-8" )
+                            .useDelimiter("\\A").next();
+    
+            JSONObject obj = (JSONObject) JSONValue.parse(jsonString);
+            
+            // check the result message
+            if( !obj.containsKey("status") )
+            {
+                System.err.println("Returned JSON message is malformed, " +
+                                    "no status: " + jsonString);
+                throw new RuntimeException("");
+            }
+            
+            String status = (String) obj.get("status");
+            if( status.compareTo("OK") != 0 )
+            {
+                System.err.println("Failed to get global scores from server ");
+                if(obj.containsKey("message"))
+                    System.err.println("Message: " + obj.get("message"));
+                throw new RuntimeException("");
+            }
+            
+            // otherwise, things look good
+            JSONArray scoreArray = (JSONArray)obj.get("scores");
+            for( Object scoreObj : scoreArray )
+            {
+                JSONObject scoreMap = (JSONObject)scoreObj;
+                
+                GlobalHighScoreRow row = new GlobalHighScoreRow();
+                row.id      = Integer.parseInt(     (String) scoreMap.get("row_id") );
+                row.nick    =                       (String) scoreMap.get("nick");
+                row.date    = Integer.parseInt(     (String) scoreMap.get("date")   );
+                row.score   = Double.parseDouble(   (String) scoreMap.get("score")  );
+                if( row.id == getInteger("lastRowId"))
+                    row.isCurrent = true;
+                scores.add(row);
+            }
+        
+        }
+        catch (MalformedURLException e)
+        {
+            System.err.println("Failed to get global scores form server");
+            e.printStackTrace(System.out);
+        } 
+        catch (IOException e)
+        {
+            System.err.println("Failed to get global scores form server");
+            e.printStackTrace(System.out);
+        }  
+        catch( RuntimeException e)
+        {
+            
+        }
+
+        
+        return scores;
     }
 }
