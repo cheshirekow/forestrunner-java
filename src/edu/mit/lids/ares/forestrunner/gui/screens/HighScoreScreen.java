f055fe6d7f08cab60a78f83fd327f224f17b86ef
Josh Bialkowski <jbialk@mit.edu>
2012-05-24 12:28:03 -0400

    Added progress bar to the highscore screen and separated data store interaction into steps.

--- src/edu/mit/lids/ares/forestrunner/gui/screens/HighScoreScreen.java
+++ src/edu/mit/lids/ares/forestrunner/gui/screens/HighScoreScreen.java
@@ -2,42 +2,107 @@ package edu.mit.lids.ares.forestrunner.gui.screens;
 
 import java.util.List;
 
+import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.NiftyEventSubscriber;
 import de.lessvoid.nifty.controls.ButtonClickedEvent;
 import de.lessvoid.nifty.controls.ListBox;
-import de.lessvoid.nifty.controls.TextField;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.screen.Screen;
 import edu.mit.lids.ares.forestrunner.Game;
 import edu.mit.lids.ares.forestrunner.data.GlobalHighScoreRow;
 import edu.mit.lids.ares.forestrunner.data.UserHighScoreRow;
 import edu.mit.lids.ares.forestrunner.gui.ScreenBase;
-import edu.mit.lids.ares.forestrunner.screens.NickChangeResult;
+import edu.mit.lids.ares.forestrunner.nifty.ProgressbarControl;
 
 public class HighScoreScreen 
     extends ScreenBase
 {
+    private int     m_step;
+    private String  m_goto;
+    
+    private ProgressbarControl  m_pb; 
+    private Element             m_commPopup;
+    
+    private List<UserHighScoreRow>      m_userList;
+    private List<GlobalHighScoreRow>    m_globallist;
+    
     public HighScoreScreen( Game game )
     {
         super(game,true,true);
     }
     
+    @Override
+    public void bind(Nifty nifty, Screen screen)
+    {
+        super.bind(nifty,screen);
+        m_commPopup = m_nifty.createPopup("pop.comm");
+        m_pb = m_commPopup.findControl("pb.comm", ProgressbarControl.class);
+    }
+    
     @SuppressWarnings("unchecked")
+    @Override
+    public void update_impl(float tpf)
+    {
+        switch(m_step)
+        {
+            case 0:
+                m_nifty.showPopup(m_screen, m_commPopup.getId(), null);
+                break;
+                
+            case 1:
+                m_dataStore.recordScore(m_game.getScore());
+                break;
+                
+            case 2:
+                m_userList    = m_dataStore.getUserScores();
+                break;
+                
+            case 3:
+                m_globallist  = m_dataStore.getGlobalScores();
+                break;
+                
+            case 4:
+            {
+                ListBox<UserHighScoreRow> listBox =(ListBox<UserHighScoreRow>) 
+                        m_screen.findNiftyControl("lb.personalHigh", ListBox.class);
+                listBox.clear();
+                System.out.println("personal scores:");
+                for( UserHighScoreRow row : m_userList )
+                {
+                    System.out.println("   " + row.date + ", " + row.score);
+                    listBox.addItem(row);
+                }
+                listBox.refresh();
+                
+                break;
+            }
+                
+            case 5:
+                // update global display;
+                break;
+                
+            case 300:
+                m_nifty.closePopup(m_commPopup.getId());
+                break;
+                
+            default:
+            {
+                if( m_goto.length() > 0 )
+                    m_nifty.gotoScreen(m_goto);
+                break;
+            }
+        }
+        
+        m_step++;
+        m_pb.setProgress( m_step/300.0f );
+    }
+    
     public void onStart_impl()
     {
-        m_dataStore.recordScore(m_game.getScore());
-        
-        List<UserHighScoreRow>      userList    = m_dataStore.getUserScores();
-        List<GlobalHighScoreRow>    globallist  = m_dataStore.getGlobalScores();
-        
-        ListBox<UserHighScoreRow> listBox =(ListBox<UserHighScoreRow>) 
-                m_screen.findNiftyControl("lb.personalHigh", ListBox.class);
-        listBox.clear();
-        System.out.println("personal scores:");
-        for( UserHighScoreRow row : userList )
-        {
-            System.out.println("   " + row.date + ", " + row.score);
-            listBox.addItem(row);
-        }
-        listBox.refresh();
+        m_step = 0;
+        m_goto = "";
+        m_mgr.attach(this);
+        m_pb.setProgress( 0.0f );
         
         /*
         Properties props = new Properties();
@@ -82,6 +147,12 @@ public class HighScoreScreen
         */
     }
     
+    @Override
+    public void onEnd_impl()
+    {
+        m_mgr.detach(this);
+    }
+    
     @NiftyEventSubscriber(pattern="highscore.btn.*")
     public void onButton( String id, ButtonClickedEvent event )
     {
@@ -89,11 +160,11 @@ public class HighScoreScreen
         if( id.compareTo("highscore.btn.again")==0 )
         {
             m_game.initRun();
-            m_nifty.gotoScreen("countdown3");
+            m_goto = "countdown3";
         }
         else if( id.compareTo("highscore.btn.settings")==0 )
         {
-            m_nifty.gotoScreen("game");
+            m_goto = "game";
         }
     }
 }
